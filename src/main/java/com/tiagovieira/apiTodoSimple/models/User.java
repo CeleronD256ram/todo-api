df1545220df0cec3dc40ea80949a437c7cbbbf34
Tiago Vieira <celerond256@outlook.com>
2024-10-16 14:56:04 -0300

    spring boot project

--- src/main/java/com/tiagovieira/apiTodoSimple/models/User.java
+++ src/main/java/com/tiagovieira/apiTodoSimple/models/User.java
@@ -1,5 +1,75 @@
 package com.tiagovieira.apiTodoSimple.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    public static final String TABLE_NAME = "user";
+
+
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 60, nullable = false, unique = true)
+    @Size(min = 5, max = 30)
+    @NotBlank
+    private String username;
+
+    @Id
+    @Column(name = "password", length = 15, nullable = false, unique = true)
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Size(min = 8, max = 30)
+    @NotBlank
+    private String password;
+
+
+    public User() {
+    }
+
+
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
 
 }
